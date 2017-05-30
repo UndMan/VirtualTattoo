7472495611c0d50909de29f47eed60adc78e1e61
Manuel <wi16m004@technikum-wien.at>
2017-05-30 13:41:14 +0200

    Test

--- app/src/main/java/com/example/manuel/virtualtattoo/MainActivity.java
+++ app/src/main/java/com/example/manuel/virtualtattoo/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.manuel.virtualtattoo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String EXTRA_MESSAGE = "com.example.manuel.virtualtattoo.LOGINSCREEN";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+            Intent intent = new Intent(MainActivity.this, LoginScreen.class);
+            startActivity(intent);
     }
 }
